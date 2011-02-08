fca2cf3903a8930a466dc7fb3ae2a2ebd1653141
Tim D. Smith <git@tim-smith.us>
2011-02-07 22:24:54 -0800

    fix sorting of layar data and add proper newlines

--- repo2layar_content.py
+++ repo2layar_content.py
@@ -12,10 +12,10 @@ def main():
     for rguid, resto in repo.items():
         f = open('layar_content/%s.html' % rguid, 'w')
         print >> f, '<h1>%(name)s</h1><p>%(street)s, %(jurisdiction)s</p>' % resto
-        for i in [j[1] for j in sorted([ (vch2datetime(resto['inspections'][k]['date']), k) for k in xrange(len(resto['inspections'])) ])]:
+        for i in reversed([j[1] for j in sorted([ (vch2datetime(resto['inspections'][k]['date']), k) for k in xrange(len(resto['inspections'])) ])]):
                 print >> f, '<h2>%s</h2>' % resto['inspections'][i]['date']
                 for finding in resto['inspections'][i]['findings']:
-                    print >> f, ('<h4>%s</h4><p>%s</p>' % (finding[0], finding[1])).encode('ascii', 'xmlcharrefreplace')
+                    print >> f, ('<h4>%s</h4><p>%s</p>' % (finding[0], finding[1])).replace('\n', '<br />').encode('ascii', 'xmlcharrefreplace')
         f.close()
 
 if __name__ == '__main__':
